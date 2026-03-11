e054625e7feaf5d10c1f286766b6dd8a358d1087
kilyanni <github@nkilian.org>
2026-03-11 04:48:51 +0100

    whisper stuff

--- src/ggml/whisper-cpp.nix
+++ src/ggml/whisper-cpp.nix
@@ -1,17 +1,30 @@
 {
+  lib,
   fetchFromGitHub,
   intel-llvm,
   cmake,
   ninja,
   oneDNN,
   oneMath,
+  syclcompat,
   tbb_2022,
-  # mkl,
   git,
   opencl-headers,
   ocl-icd,
+  rocmSupport ? false,
+  cudaSupport ? false,
+  rocmPackages ? {},
 }: let
-  version = "unstable-2025-09-19";
+  version = "1.8.3";
+  syclTarget =
+    if rocmSupport
+    then "AMD"
+    else if cudaSupport
+    then "NVIDIA"
+    else "INTEL";
+  rocmGpuTargets =
+    lib.optionalString (rocmPackages ? clr.gpuTargets)
+    (builtins.concatStringsSep "," rocmPackages.clr.gpuTargets);
 in
   intel-llvm.stdenv.mkDerivation {
     pname = "whisper-cpp";
@@ -20,10 +33,13 @@ in
     src = fetchFromGitHub {
       owner = "ggml-org";
       repo = "whisper.cpp";
-      # tag = "v${version}";
-      rev = "44fa2f647cf2a6953493b21ab83b50d5f5dbc483";
-      hash = "sha256-1j8Z/fuxGkb3h21nwRJQ3HOS2/TfdjGo5Kaz41AM8js=";
+      tag = "v${version}";
+      hash = "sha256-TeS1lGKEzkHOoBemy/tMGtIsy0iouj9DTYIgTjUNcQk=";
     };
+
+    patches = [
+      ./patches/sycl-amd-multi-arch.patch
+    ];
 
     nativeBuildInputs = [
       cmake
@@ -32,22 +48,35 @@ in
     ];
 
     buildInputs = [
-      oneDNN
+      # oneDNN
       oneMath
+      syclcompat
       tbb_2022
-      # mkl
       opencl-headers
       ocl-icd
-      # llvm.baseLlvm.openmp
     ];
+
+    postPatch = lib.optionalString (syclTarget == "INTEL") ''
+      substituteInPlace ggml/src/ggml-sycl/CMakeLists.txt \
+        --replace-fail "find_package(MKL REQUIRED)" "find_package(oneMath REQUIRED)" \
+        --replace-fail "target_link_libraries(ggml-sycl PRIVATE MKL::MKL_SYCL::BLAS)" \
+                       "target_link_libraries(ggml-sycl PRIVATE ONEMATH::onemath)" \
+        --replace-fail "target_compile_definitions(ggml-sycl PRIVATE GGML_SYCL_USE_INTEL_ONEMKL)" \
+                       "target_compile_definitions(ggml-sycl PRIVATE GGML_SYCL_GENERIC)"
+    '';
 
     hardeningDisable = [
       "zerocallusedregs"
       "pacret"
-      # "shadowstack"
     ];
 
-    cmakeFlags = [
-      "-DWHISPER_SYCL=ON"
-    ];
+    cmakeFlags =
+      [
+        (lib.cmakeBool "GGML_SYCL" true)
+        (lib.cmakeBool "GGML_NATIVE" false)
+        (lib.cmakeFeature "GGML_SYCL_TARGET" syclTarget)
+      ]
+      ++ lib.optionals (syclTarget == "AMD" && rocmGpuTargets != "") [
+        (lib.cmakeFeature "GGML_SYCL_DEVICE_ARCH" rocmGpuTargets)
+      ];
   }
