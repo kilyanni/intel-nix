f37a0964570d57aa310256c81d68056c8e31ba5f
kilyanni <github@nkilian.org>
2026-03-11 06:39:24 +0100

    test builds for stdenv based on toolkitss

--- toolkits/default.nix
+++ toolkits/default.nix
@@ -6,4 +6,17 @@
   };
 
   installer = callPackage ./installer {inherit deps;};
+
+  tests = let
+    intel-llvm = {stdenv = installer.base.passthru.stdenv;};
+    oneMath-sycl-blas = callPackage ../src/onemath-sycl-blas.nix {inherit intel-llvm;};
+    oneMath = callPackage ../src/onemath.nix {inherit intel-llvm oneMath-sycl-blas;};
+    oneDNN = callPackage ../src/onednn.nix {inherit intel-llvm;};
+    syclcompat = callPackage ../src/syclcompat.nix {};
+  in {
+    inherit oneMath-sycl-blas oneMath oneDNN;
+    whisper-cpp = callPackage ../src/ggml/whisper-cpp.nix {
+      inherit intel-llvm oneMath oneDNN syclcompat;
+    };
+  };
 }
