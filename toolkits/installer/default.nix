16d29dbc4523c518269fe7784dec242bb29f7982
kilyanni <github@nkilian.org>
2026-03-21 03:43:17 +0100

    drop toolkit in favor of nixpkg ver

--- toolkits/installer/default.nix
+++ toolkits/installer/default.nix
@@ -1,13 +1,12 @@
 {
   callPackage,
-  deps,
+  intel-oneapi,
 }: rec {
   base = callPackage ./wrapper.nix {
-    kit = callPackage ./base.nix {inherit deps;};
+    kit = intel-oneapi.base;
   };
 
-  # hpc.nix uses base.makeKit (via passthru) to share installer.nix logic
   hpc = callPackage ./wrapper.nix {
-    kit = callPackage ./hpc.nix {inherit base;};
+    kit = intel-oneapi.hpc;
   };
 }
--- toolkits/installer/hpc.nix
+++
@@ -1,7 +0,0 @@
-{base}:
-base.makeKit {
-  kit = "hpc";
-  version = "2025.3.1.55";
-  uuid = "f59a79aa-4a6e-46e8-a6c2-be04bb13f274";
-  sha256 = "sha256-NwLR7OWzmKYM05BZ2U7IZI+3IBnb0wiUgF+vHrX0Tro=";
-}
